1ce163a5d529dbd47e64170aecfd09727f4e2f3f
Ian Young <iyoung@outboxaxs.com>
2017-01-01 19:37:32 -0800

    removed quotations from wifi credentials

--- wifi.lua
+++ wifi.lua
@@ -4,9 +4,12 @@ if not ok then
   print(e)
   return
 end
+print(ssid)
+print(password)
+print(apikey)
 print("Setting up WIFI...")
 wifi.setmode(wifi.STATION)
-wifi.sta.config(" .. ssid .. "," .. password ..")
+wifi.sta.config(ssid,password)
 wifi.sta.connect()
 tmr.alarm(1, 1000, 1, function() 
   print("in function")
